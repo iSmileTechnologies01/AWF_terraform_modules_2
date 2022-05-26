86e1c92de5391dcec0401ce2964809139c75f97f
“eben-ismile” <ebenezer@ismiletechnologies.com>
2022-05-26 15:28:03 +0000

    Lambda Complete

--- CORE/Lambda/main.tf
+++ CORE/Lambda/main.tf
@@ -1,25 +1,21 @@
 data "aws_iam_role" "example" {
-  name = "IamRole"
+  name = var.rolename
 }
 
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  filename      = var.filename
+  function_name = var.function_name
+  role          = data.aws_iam_role.example.arn
+  handler       = var.handler
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = filebase64sha256(var.filename)
 
-  runtime = "nodejs12.x"
+  runtime = var.runtime
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
+  
 }
---
+++ CORE/Lambda/variable.tf
@@ -0,0 +1,26 @@
+variable "filename" {
+    default="lambda_function_payload.zip"
+    type = string
+  
+}
+
+variable "runtime" {
+    default="nodejs12.x"
+    type = string
+  
+}
+variable "function_name" {
+    default="lambda_function_name"
+    type = string
+}
+
+variable "function_name" {
+    default="index.test"
+    type = string
+}
+
+variable "rolename" {
+    default="IamRole"
+    type = string
+}
+
