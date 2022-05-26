f9af5385e523fb1f6d5b6a2180e03706cc0f2688
“eben-ismile” <ebenezer@ismiletechnologies.com>
2022-05-26 14:32:03 +0000

    S3 completed

---
+++ CORE/VPC/main.tf
@@ -0,0 +1,55 @@
+#vpc creation
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+#Created internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+#creating routetable
+resource "aws_route_table" "route-private" {
+  vpc_id = aws_vpc.vpc.id
+    route {
+    cidr_block = var.route_cidr_block
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+   route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id             = aws_internet_gateway.igw.id
+  }
+ 
+
+}
+
+# Create a pvt-subnets main1,main2
+
+resource "aws_subnet" "pvt_main1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.pvt1_cidr_block
+  availability_zone = var.pvt1_availability_zone
+
+}
+
+# Create a private-subnet2
+resource "aws_subnet" "pvt_main2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "172.35.2.0/24"
+  availability_zone = "us-east-1b"
+
+  
+}
+
+
+#Associate subnet with route table
+resource "aws_route_table_association" "route-private-main1" {
+  subnet_id      = aws_subnet.pvt_main1.id
+  route_table_id = aws_route_table.route-private.id
+}
+resource "aws_route_table_association" "route-private-main2" {
+  subnet_id      = aws_subnet.pvt_main2.id
+  route_table_id = aws_route_table.route-private.id
+}
---
+++ CORE/VPC/variable.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr_block" {
+    type = string
+    default = "172.35.0.0/16"
+}
+
+
+variable "route_cidr_block" {
+    type = string
+    default = "0.0.0.0/0"
+
+
+}
+
+
+variable "pvt1_cidr_block" {
+    type = string
+    default = "172.35.1.0/24"
+
+}
+variable "pvt1_availability_zone" {
+    type = string
+    default = "us-east-1a"
+
+}
+
